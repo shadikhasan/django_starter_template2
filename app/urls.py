e933bc2aa7d9dab433f214073f90d464e0d568df
SHADIK <shadik.sk420@gmail.com>
2024-09-11 03:43:55 +0600

    updated

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views  # Import views from the current app
 
 urlpatterns = [
-    
+    path('app/', views.testapp, name="testapp"), 
 ]
